d3a962367b3dc685a7ca1e0aaa8e8ef5d48a1286
mdshajibazher <mdshajibazher@gmail.com>
2022-10-23 14:25:29 +0600

    balance feature added

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import {useSelector} from "react-redux";
 
 function Balance(props) {
+    const {transactions} =    useSelector(state => state.transaction) || {};
+
+    const clculateIncome = () => {
+        let income = 0;
+        transactions.forEach(txn => {
+            const {type, amount} = txn;
+            type === 'income' ? income+= amount : income-= amount;
+        })
+        return income;
+    }
     return (
         <div className="top_card">
             <p>Your Current Balance</p>
             <h3>
                 <span>৳</span>
-                <span>10500</span>
+                {transactions.length > 0 ?   <span>{clculateIncome(transactions)}</span> : 0 }
             </h3>
         </div>
     );
