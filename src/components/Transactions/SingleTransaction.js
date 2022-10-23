fe2e7a91b7948c4854701b8ab20afc632079d3a8
mdshajibazher <mdshajibazher@gmail.com>
2022-10-23 13:12:26 +0600

    delete feature added

--- src/components/Transactions/SingleTransaction.js
+++ src/components/Transactions/SingleTransaction.js
@@ -2,7 +2,7 @@ import React from 'react';
 import deleteImage from "../../assets/images/delete.svg";
 import editImage from "../../assets/images/edit.svg";
 import {useDispatch} from "react-redux";
-import {editActive} from "../../features/transaction/transactionSlice";
+import {editActive, fetchTransactions, removeTransaction} from "../../features/transaction/transactionSlice";
 
 
 function SingleTransaction({transaction}) {
@@ -12,13 +12,19 @@ function SingleTransaction({transaction}) {
     const handleEdit =  () => {
         dispatch(editActive(transaction));
     }
+
+    const handleDelete = ({id}) => {
+        dispatch(removeTransaction(id));
+        dispatch(fetchTransactions());
+    }
     return (
 
         <li className={`transaction ${transaction.type }`}>
             <p>{transaction.name}</p>
             <div className="right">
                 <p>৳ {transaction.amount}</p>
-                <button className="link">
+                <button className="link"  onClick={ () => handleDelete(transaction)}>
+
                     <img
                         className="icon"
                         src={deleteImage}
